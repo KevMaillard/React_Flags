25d5b5003a2f51786a9e0c8d5bb4957720a1a3ce
KevMaillard <86302957+KevMaillard@users.noreply.github.com>
2022-03-22 04:23:01 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ratione a reiciendis quidem necessitatibus eius delectus mollitia sit, consectetur inventore unde quae laudantium maxime! Commodi veniam quis iusto eligendi. Sed reprehenderit consequatur odit vero nemo natus eaque saepe, adipisci blanditiis exercitationem, voluptates voluptate unde labore, error sunt quis expedita accusantium ratione consequuntur veniam nam beatae nisi facilis. Quam soluta ipsa distinctio natus quod, aspernatur eaque est ducimus dolores repellendus suscipit laboriosam exercitationem labore voluptatibus nesciunt praesentium dignissimos ex vel architecto repudiandae, adipisci laborum! Reiciendis, cum similique! Laboriosam aliquid vitae voluptatibus officiis ullam, voluptates aliquam illo, cupiditate animi dicta nostrum beatae.
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam unde, rem laudantium numquam at provident perferendis quam magnam rerum necessitatibus id quisquam similique quibusdam fugit aperiam cum quae tempore tempora consequatur, cupiditate vel eum nobis, eos aliquam? Cum dicta iusto omnis, vitae autem provident, aliquam blanditiis voluptatibus maiores optio dolorem magnam voluptates ut eius, consequuntur modi doloremque! Ipsum quasi at quidem iusto earum saepe assumenda maiores nemo, voluptatum, eveniet hic consequatur omnis distinctio a, quos perferendis sunt debitis soluta nostrum. Odio voluptatibus eius, placeat veritatis rem asperiores magni est iusto! Tempore qui expedita fugit numquam reiciendis voluptatibus laborum non adipisci?
+            </p>
+        </div>
+    );
+};
+
+export default About;
